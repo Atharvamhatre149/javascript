d6b3be2e56d90646d4db6bd79f373094927a466f
Atharvamhatre149 <89691657+Atharvamhatre149@users.noreply.github.com>
2023-11-30 02:08:18 +0000

    dataTypes

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) we are using nodeJS, not browser
+
+let name="Atharva";
+let age=20;
+let isLoggedIn= false;
+
+//number=> 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => value is not assigned yet
+//symbol => unique
+//object
+
+console.log(typeof(null));  // it is object
+console.log(typeof(undefined)); //it is undefined only
+console.log(typeof(isLoggedIn)); 
